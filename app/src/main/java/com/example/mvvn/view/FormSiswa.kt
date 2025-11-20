c19b173594ba8106f35e0c776b51ad1c140d8267
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 15:21:52 +0700

    add spacer untuk memberikan jarak dengan komponen atasnya setinggi 20.dp

--- app/src/main/java/com/example/mvvn/view/FormSiswa.kt
+++ app/src/main/java/com/example/mvvn/view/FormSiswa.kt
@@ -4,9 +4,13 @@ import android.graphics.Paint
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -106,5 +110,7 @@ fun FormSiswa(
                 textAlamat = it
             }
         )
+        Spacer(modifier= Modifier .height(20.dp))
+
     }
 }
