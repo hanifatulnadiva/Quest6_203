813ccc50ac317c8c68778f724d9ca99ff2c4f3aa
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 15:54:33 +0700

    Add Column di Body Scaffold terus mengatur padding, terus mengatur komponennya memiliki jarak satu sama lain

--- app/src/main/java/com/example/mvvn/view/TampilSiswa.kt
+++ app/src/main/java/com/example/mvvn/view/TampilSiswa.kt
@@ -1,5 +1,8 @@
 package com.example.mvvn.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,6 +33,13 @@ fun TampilSiswa(
             title = { Text(text=stringResource(R.string.detail), color= Color.White)},
             colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(R.color.purple_500))
         )}
+    ){
+        isiRuang->
+        Column (modifier = Modifier
+            .padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
 
-    ){  }
+        }
+    }
 }
