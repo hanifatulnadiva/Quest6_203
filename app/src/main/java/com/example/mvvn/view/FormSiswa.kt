b29d89d68c85ebe4093e6a0270b58e8ef455b1ed
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 09:39:56 +0700

    add variabel untuk menyimpan inputan itu ada text nama, text alamat, adan text gender alalu juga namabahin val untuk menyimpan list datanya

--- app/src/main/java/com/example/mvvn/view/FormSiswa.kt
+++ app/src/main/java/com/example/mvvn/view/FormSiswa.kt
@@ -2,6 +2,10 @@ package com.example.mvvn.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @OptIn (ExperimentalMaterial3Api::class)
@@ -11,5 +15,8 @@ fun FormSiswa(
     onSubmitButtonClick:(MutableList<String>)->Unit,
     modifier: Modifier=Modifier
     ){
-
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textGender by remember { mutableStateOf(value = "") }
+    val listData: MutableList<String> = mutableListOf(textNama, textAlamat, textGender)
 }
