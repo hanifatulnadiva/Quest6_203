7515e6798d5cb0b85fe6771cfe9c049a3d7f019d
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 11:49:20 +0700

    add background untuk top app bar nya dengan resource color

--- app/src/main/java/com/example/mvvn/view/FormSiswa.kt
+++ app/src/main/java/com/example/mvvn/view/FormSiswa.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,6 +12,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.mvvn.R
 
@@ -26,6 +28,7 @@ fun FormSiswa(
     var textGender by remember { mutableStateOf(value = "") }
     val listData: MutableList<String> = mutableListOf(textNama, textAlamat, textGender)
     Scaffold (modifier= Modifier,
-        topBar = { TopAppBar(title = { Text(stringResource(R.string.app_name), color= Color.White)})
+        topBar = { TopAppBar(title = { Text(stringResource(R.string.app_name), color= Color.White)},
+            colors= TopAppBarDefaults.mediumTopAppBarColors(colorResource(R.color.purple_500)))
         }){}
 }
