2e297b90e59919e7e56d87022ba3a098d33ef477
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 16:33:37 +0700

    untuk menampilkan halaman FormSiswa ketika route Formulir dibuka. Di dalamnya, aplikasi mengambil teks pilihan jenis kelamin dari val jenisK yang ada di objekClass, lalu mengirimkannya ke komponen form. Saat tombol submit ditekan, data siswa yang diisi user dikirim ke viewModel dan halaman akan berpindah ke tampilan detail menggunakan navController.navigate

--- app/src/main/java/com/example/mvvn/view/uicontroller/navigasi.kt
+++ app/src/main/java/com/example/mvvn/view/uicontroller/navigasi.kt
@@ -2,18 +2,22 @@ package com.example.mvvn.view.uicontroller
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.Navigation
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mvvn.viewmodel.SiswaViewModel
 import com.example.mvvn.view.TampilSiswa
 import com.example.mvvn.view.FormSiswa
+import com.example.mvvn.model.DataJK.JenisK
 
 enum class Navigasi{
     Formulir,
@@ -34,7 +38,16 @@ fun SiswaApp(
             startDestination = Navigasi.Formulir.name,
             modifier= Modifier.padding(paddingValues = isiRuang)
         ){
-
+            composable (route = Navigasi.Formulir.name)
+            {
+                val konteks = LocalContext.current
+                FormSiswa(
+                    pilihanJK = JenisK.map { id -> konteks.resources.getString(id) }, onSubmitButtonClick = {
+                        viewModel.setSiswa(it)
+                        navController.navigate (route= Navigasi.Detail.name)
+                    }
+                )
+            }
         }
     }
 }
