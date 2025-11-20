3d8c782a619140d4554e454351bbba21119ed3ee
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2025-11-20 15:51:27 +0700

    Add background untuk topbarnya

--- app/src/main/java/com/example/mvvn/view/TampilSiswa.kt
+++ app/src/main/java/com/example/mvvn/view/TampilSiswa.kt
@@ -4,9 +4,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.mvvn.model.Siswa
 import com.example.mvvn.R
@@ -25,7 +27,8 @@ fun TampilSiswa(
     )
     Scaffold (modifier = Modifier,
         topBar = {TopAppBar(
-            title = { Text(text=stringResource(R.string.detail), color= Color.White) }
+            title = { Text(text=stringResource(R.string.detail), color= Color.White)},
+            colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(R.color.purple_500))
         )}
 
     ){  }
